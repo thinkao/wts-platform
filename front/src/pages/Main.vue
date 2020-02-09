4c5880311ec4f9c87a32199001cebcb1e75d0753
chen <chenshaobo272@gmail.com>
2020-02-09 18:02:03 +0800

    main page frame

--- front/src/pages/Main.vue
+++ front/src/pages/Main.vue
@@ -1,6 +1,27 @@
 <template >
   <div class="body">
-    <span>这是首页</span>
+    <div class="content">
+      <div class="carousel">
+        轮播图
+      </div>
+      <div class="news">
+        <div class="news-first">
+          news1
+        </div>
+        <div class="news-second">
+          news2
+        </div>
+        <div class="news-third">
+          news3
+        </div>
+      </div>
+      <div class="dynamic">
+        动态
+      </div>
+    </div>
+    <div class="person-info">
+      占位<br>占位<br>占位<br>占位<br>占位<br>占位<br>hahahah
+    </div>
   </div>
 </template>
 
@@ -11,6 +32,34 @@ export default {
 </script>
 <style scoped>
   .body{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
     background-color:#f6f6f8;
+  }
+  .content{
+    border: 1px solid black;
+    width: 70%;
+  }
+  .person-info{
+    width: 20%;
+    border: 1px solid black;
+  }
+
+  .content .carousel{
+
+  }
+  .content .news{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+  }
+  .content .dynamic{
+
+  }
+
+  .content .news .news-first,.content .news .news-second,.content .news .news-third{
+    border: 1px solid black;
+    flex-grow: 1;
   }
 </style>
